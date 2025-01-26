5346a49e0d4b9c7df0a0647d5687d60d83425c03
Amos Wolfe <amos@dropbearbytes.com>
2025-01-26 11:52:29 +0800

    Only played sfx when hitting the right surfaces

--- Assets/Scripts/Audio/PlaySfxOnFirstImpact.cs
+++ Assets/Scripts/Audio/PlaySfxOnFirstImpact.cs
@@ -6,13 +6,30 @@ public class PlaySfxOnFirstImpact : MonoBehaviour
     private AudioClip clip;
     [SerializeField]
     private float pitchRandomness = 0.25f;
-
+    [SerializeField]
+    private string tagRequired = "Liquid";
+    [SerializeField]
+    private float maxDelay = 2.5f;
 
     private bool playedSound;
+    private float spawnTime;
+
+    private void OnEnable()
+    {
+        spawnTime = Time.time;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(playedSound)
+        {
+            return;
+        }
+        if(Time.time - spawnTime > maxDelay)
+        {
+            return;
+        }
+        if (!collision.transform.tag.Equals(tagRequired))
         {
             return;
         }
--- Assets/Scripts/Audio/PlaySfxOneAtATimeOnImpact.cs
+++ Assets/Scripts/Audio/PlaySfxOneAtATimeOnImpact.cs
@@ -6,12 +6,30 @@ public class PlaySfxOneAtATimeOnImpact : MonoBehaviour
     private AudioClip clip;
     [SerializeField]
     private float pitchRandomness = 0.25f;
+    [SerializeField]
+    private string tagRequired = "BobaCup";
+    [SerializeField]
+    private float maxDelay = 2.5f;
 
     private bool playedSound;
+    private float spawnTime;
+
+    private void OnEnable()
+    {
+        spawnTime = Time.time;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (playedSound)
+        {
+            return;
+        }
+        if (Time.time - spawnTime > maxDelay)
+        {
+            return;
+        }
+        if (!collision.transform.tag.Equals(tagRequired))
         {
             return;
         }
