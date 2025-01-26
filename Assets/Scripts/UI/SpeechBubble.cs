9c2de1d42fc8102949c83f57daf1de8138c0cf61
Amos Wolfe <amos@dropbearbytes.com>
2025-01-26 10:09:04 +0800

    Added ticker text animation to dialogue

--- Assets/Scripts/UI/SpeechBubble.cs
+++ Assets/Scripts/UI/SpeechBubble.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -6,6 +7,9 @@ public class SpeechBubble : MonoBehaviour
     [SerializeField]
     private TMP_Text text;
 
+    [SerializeField]
+    private float delayPerCharacter = 0.02f;
+
     private void Awake()
     {
         text.text = string.Empty;
@@ -13,12 +17,39 @@ public class SpeechBubble : MonoBehaviour
 
     public void Hide()
     {
+        StopAllCoroutines();
         gameObject.SetActive(false);
     }
 
     public void ShowDialogue(string text)
     {
         gameObject.SetActive(true);
+        this.text.text = string.Empty;
+
+        StopAllCoroutines();
+        StartCoroutine(ShowAnimation(text));
+    }
+
+    private IEnumerator ShowAnimation(string text)
+    {
+        if(text.Length == 0)
+        {
+            yield break;
+        }
+
+        float totalTime = text.Length * delayPerCharacter;
+        float t = 0;
+        while (t < 1)
+        {
+            t = Mathf.Clamp01(t + Time.deltaTime / totalTime);
+
+            int characters = Mathf.RoundToInt(text.Length * t);
+            string visibleText = text.Substring(0, characters);
+            this.text.text = visibleText;
+
+            yield return null;
+        }
+
         this.text.text = text;
     }
 }
